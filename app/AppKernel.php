b6b66ad428b99a463d535ff83d01691be27a0523
Jānis Gruzis <jgruzis@gmail.com>
2015-04-15 02:21:53 +0300

    API cors

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 			new JMS\SerializerBundle\JMSSerializerBundle(),
+			new Nelmio\CorsBundle\NelmioCorsBundle(),
 
             new Frontend\AppBundle\FrontendAppBundle(),
             new Backend\UserBundle\BackendUserBundle(),
