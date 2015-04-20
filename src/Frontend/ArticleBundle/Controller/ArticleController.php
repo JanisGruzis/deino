622f5c003ca7a3abb4b362659304887e1afd27b3
Inwhite <aleksandrs.rilins@gmail.com>
2015-04-20 17:55:20 +0300

    updated popular method

--- src/Frontend/ArticleBundle/Controller/ArticleController.php
+++ src/Frontend/ArticleBundle/Controller/ArticleController.php
@@ -11,122 +11,118 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends BaseController
 {
-	/**
-	 * @Route("/api/categories", name="api_categories")
-	 */
-	public function categoriesAction()
-	{
-		$categroyRepository = $this->get('repository.category');
-		$categories = $categroyRepository->getCategories();
+    /**
+     * @Route("/api/categories", name="api_categories")
+     */
+    public function categoriesAction()
+    {
+        $categroyRepository = $this->get('repository.category');
+        $categories = $categroyRepository->getCategories();
 
-		return new JsonResponse($categories);
-	}
+        return new JsonResponse($categories);
+    }
 
-	/**
-	 * @Route("/api/sources", name="api_sources")
-	 */
-	public function sourcesAction()
-	{
-		$sourceRepository = $this->get('repository.source');
-		$sources = $sourceRepository->getSources();
+    /**
+     * @Route("/api/sources", name="api_sources")
+     */
+    public function sourcesAction()
+    {
+        $sourceRepository = $this->get('repository.source');
+        $sources = $sourceRepository->getSources();
 
-		return new JsonResponse($sources);
-	}
+        return new JsonResponse($sources);
+    }
 
-	/**
-	 * @Route("/api/popular/{period}", name="api_popular")
-	 */
-	public function sourcesAction($period)
-	{
-		$test = [$period];
+    /**
+     * @Route("/api/popular/{period}", name="api_popular")
+     */
+    public function popularArticles($period)
+    {
+        $test = [$period];
 
-		return new JsonResponse($test);
-	}
+        return new JsonResponse($test);
+    }
 
-	/**
-	 * @Route("/api/articles_by_category", name="api_articles_by_category")
-	 */
-	public function articlesByCategoryAction(Request $request)
-	{
-		$articleRepository = $this->get('repository.article');
-		$clusterRepository = $this->get('repository.cluster');
+    /**
+     * @Route("/api/articles_by_category", name="api_articles_by_category")
+     */
+    public function articlesByCategoryAction(Request $request)
+    {
+        $articleRepository = $this->get('repository.article');
+        $clusterRepository = $this->get('repository.cluster');
 
-		$clusters = $clusterRepository->getClusters([], 'category_id', 3);
-		$clusterIds = array_map(function($item){
-			return $item['id'];
-		}, $clusters);
+        $clusters = $clusterRepository->getClusters([], 'category_id', 3);
+        $clusterIds = array_map(function ($item) {
+            return $item['id'];
+        }, $clusters);
 
-		$articles = $articleRepository->getArticlesOfClusters($clusterIds);
-		$articleMap = $this->mapArticles($articles);
+        $articles = $articleRepository->getArticlesOfClusters($clusterIds);
+        $articleMap = $this->mapArticles($articles);
 
-		$result = [];
-		foreach ($clusters as $cluster)
-		{
-			$cluster['articles'] = (isset($articleMap[$cluster['id']]) ? $articleMap[$cluster['id']] : []);
-			if (!isset($result[$cluster['category_id']]))
-			{
-				$result[$cluster['category_id']] = [];
-			}
-			$result[$cluster['category_id']][] = $cluster;
-		}
+        $result = [];
+        foreach ($clusters as $cluster) {
+            $cluster['articles'] = (isset($articleMap[$cluster['id']]) ? $articleMap[$cluster['id']] : []);
+            if (!isset($result[$cluster['category_id']])) {
+                $result[$cluster['category_id']] = [];
+            }
+            $result[$cluster['category_id']][] = $cluster;
+        }
 
-		return new JsonResponse($result);
-	}
+        return new JsonResponse($result);
+    }
 
-	/**
-	 * @Route("/api/category_clusters", name="api_category_clusters")
-	 */
-	public function categoryClustersAction(Request $request)
-	{
-		$articleRepository = $this->get('repository.article');
-		$clusterRepository = $this->get('repository.cluster');
+    /**
+     * @Route("/api/category_clusters", name="api_category_clusters")
+     */
+    public function categoryClustersAction(Request $request)
+    {
+        $articleRepository = $this->get('repository.article');
+        $clusterRepository = $this->get('repository.cluster');
 
-		$query = $request->query->all();
-		$clusters = $clusterRepository->getClusters($query);
-		$clusterIds = array_map(function($item){
-			return $item['id'];
-		}, $clusters);
+        $query = $request->query->all();
+        $clusters = $clusterRepository->getClusters($query);
+        $clusterIds = array_map(function ($item) {
+            return $item['id'];
+        }, $clusters);
 
-		$articles = $articleRepository->getArticlesOfClusters([
-			'clusters' => $clusterIds
-		]);
-		$articleMap = $this->mapArticles($articles);
+        $articles = $articleRepository->getArticlesOfClusters([
+            'clusters' => $clusterIds
+        ]);
+        $articleMap = $this->mapArticles($articles);
 
-		foreach ($clusters as $key => $cluster) {
-			$clusters[$key]['articles'] = (isset($articleMap[$cluster['id']]) ? $articleMap[$cluster['id']] : []);
-		}
+        foreach ($clusters as $key => $cluster) {
+            $clusters[$key]['articles'] = (isset($articleMap[$cluster['id']]) ? $articleMap[$cluster['id']] : []);
+        }
 
-		return new JsonResponse($clusters);
-	}
+        return new JsonResponse($clusters);
+    }
 
-	/**
-	 * @Route("/api/articles", name="api_articles")
-	 */
-	public function articlesAction(Request $request)
-	{
-		$articleRepository = $this->get('repository.article');
-		$query = $request->query->all();
-		$articles = $articleRepository->getArticles($query);
+    /**
+     * @Route("/api/articles", name="api_articles")
+     */
+    public function articlesAction(Request $request)
+    {
+        $articleRepository = $this->get('repository.article');
+        $query = $request->query->all();
+        $articles = $articleRepository->getArticles($query);
 
-		return new JsonResponse($articles);
-	}
+        return new JsonResponse($articles);
+    }
 
-	/**
-	 * @param $articles
-	 * @return array
-	 */
-	private function mapArticles($articles)
-	{
-		$articleMap = [];
-		foreach ($articles as $article)
-		{
-			if (!isset($articleMap[$article['cluster_id']]))
-			{
-				$articleMap[$article['cluster_id']] = [];
-			}
-			$articleMap[$article['cluster_id']][] = $article;
-		}
+    /**
+     * @param $articles
+     * @return array
+     */
+    private function mapArticles($articles)
+    {
+        $articleMap = [];
+        foreach ($articles as $article) {
+            if (!isset($articleMap[$article['cluster_id']])) {
+                $articleMap[$article['cluster_id']] = [];
+            }
+            $articleMap[$article['cluster_id']][] = $article;
+        }
 
-		return $articleMap;
-	}
+        return $articleMap;
+    }
 }
